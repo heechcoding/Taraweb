d396e88026f7842bd8d7dc379a5be7304b9debd6
heech912 <34178802+heech912@users.noreply.github.com>
2020-08-11 20:58:57 +0900

    day1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
 
 function App() {
   return (
+    <div>
     <p> 안녕하 세요 ! </p>
+    <Button Primary> hi </Button>
+    <Progress percent = {33} indicating/>
+    </div>
   );
 }
 
