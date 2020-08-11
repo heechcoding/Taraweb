2cde8f15d46c27e8132b691a0b0b6d049bc6cfba
heech912 <34178802+heech912@users.noreply.github.com>
2020-08-11 21:31:42 +0900

    asdfdsg

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {Button, Progress} from 'semantic-ui-react'
 function App() {
   return (
     <div>
-    <p> 안녕 하 세 요 ! </p>
+    <p> 안 녕 하 세 요 ! </p>
     <Button Primary> hi </Button>
     <Progress percent = {33} indicating/>
     </div>
